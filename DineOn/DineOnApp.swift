939b399df96f99f6b554accae69786851cab5ac2
iTechEverything <omchachad12@gmail.com>
2025-10-17 01:14:06 -0700

    Initial Commit

---
+++ DineOn/DineOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineOnApp.swift
+//  DineOn
+//
+//  Created by Om Chachad on 10/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DineOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
